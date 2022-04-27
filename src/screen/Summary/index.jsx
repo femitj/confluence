05ac275903b654fa1d1b6057e9cb2cb2a7b3c66b
Tijani Femi <olutimedia@gmail.com>
2022-04-27 14:01:36 +0100

    validate age input

--- src/screen/Summary/index.jsx
+++ src/screen/Summary/index.jsx
@@ -4,11 +4,11 @@ const Summary = ({ data, premium, handlePremium }) => {
   return (
     <div className='summary'>
       <h3>{data?.name},</h3>
-      <p>Name : {data?.name}</p>
-      <p>Age : {data?.age}</p>
-      <p>Where do you live : {data?.country}</p>
-      <p>Package : {data?.package}</p>
-      <p>Premium : {!premium ? 0 : `${handlePremium()} ${data?.currency}`}</p>
+      <p>Name: {data?.name}</p>
+      <p>Age: {data?.age}</p>
+      <p>Where do you live: {data?.country}</p>
+      <p>Package: {data?.package}</p>
+      <p>Premium: {!premium ? 0 : `${handlePremium()} ${data?.currency}`}</p>
     </div>
   );
 };
